1e9a5847e0caf56120567fb3a7380f08530dd946
Sk-islam <138716556+Sk-islam@users.noreply.github.com>
2024-06-02 01:56:37 +0530

    Update whether.js

--- whether.js
+++ whether.js
@@ -71,12 +71,9 @@
 
 setInterval(()=>{
         let currentTime = new Date();
-        let time = currentTime.toLocaleString();
-        clock.innerHTML =time;
-    
-    // hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
-    // min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
-    // sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+    hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
     
     },1000)
 
